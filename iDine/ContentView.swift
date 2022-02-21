52b6f2287ef6d4979b20440dcdf168074a750ba2
Kirill <kirillfedchenko89@gmail.com>
2022-02-21 10:54:29 +0200

    New files at idine folder

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -14,6 +14,36 @@ struct ContentView: View {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
