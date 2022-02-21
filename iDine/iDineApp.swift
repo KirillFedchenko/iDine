03dcfd92a99bfa4ad8b9995feecbf856e83dce0a
Kirill <kirillfedchenko89@gmail.com>
2022-02-21 10:26:41 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Mark Bear on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
